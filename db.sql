84907176da9024e90e1cc041a87b36c8ffe2779c
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-05-29 16:19:55 +0300

    TVL

--- db.sql
+++ db.sql
@@ -74,3 +74,94 @@ CREATE TABLE public.dex_pair (
 	sync_id varchar NULL,
 	CONSTRAINT dex_pair_pkey PRIMARY KEY (id)
 );
+
+
+CREATE TABLE public.tvl_history (
+	id serial,
+	build_time timestamp NULL,
+	pool varchar NULL,
+	token varchar NULL,
+	reserve decimal(40, 0),
+	CONSTRAINT tvl_history_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE public.usd_tokens (
+	"token" varchar NOT NULL
+);
+CREATE UNIQUE INDEX usd_tokens_token_idx ON public.usd_tokens ("token");
+
+
+-- views
+create or replace view venomics.dex_reserves
+as
+select dp.id, to_timestamp(se.created_at) as update_time , se.reserve0 , se.reserve1 , 
+ti0.decimals  as token0_decimals,
+ti1.decimals  as token1_decimals,
+ti0.symbol  as token0_symbol,
+ti1.symbol  as token1_symbol,
+dp.token0, dp.token1 from dex_pair dp
+join sync_event se on dp.sync_id  = se.id 
+join tokens_info ti0 on ti0.id = dp.token0 
+join tokens_info ti1 on ti1.id = dp.token1
+
+create or replace view venomics.token_info
+as select * from public.tokens_info ti 
+
+create or replace view venomics.dex_swaps
+as
+select to_timestamp(ee.created_at) as time, sender as 	, 
+src_token , ti1.symbol  as src_token_symbol, src_amount, ti1.decimals  as src_decimals,
+dst_token , ti2.symbol  as dst_token_symbol, dst_amount, ti2.decimals  as dst_decimals
+from exchange_event ee 
+join tokens_info ti1 on ti1.id = src_token
+join tokens_info ti2 on ti2.id = dst_token
+
+
+create or replace view venomics.tvl_current
+as
+with native_based_pairs_raw as (
+  select id, (reserve0  / pow(10, token0_decimals)) as tvl_native, token0 as token from venomics.dex_reserves r
+  where r.token0 = '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+  union all
+  select id, (reserve0  / pow(10, token0_decimals)) as tvl_native, token1 as token from venomics.dex_reserves r
+  where r.token0 = '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+  union all 
+  select id, (reserve1  / pow(10, token1_decimals)) as tvl_native, token0 as token from venomics.dex_reserves r
+  where r.token1 = '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+  union all 
+  select id, (reserve1  / pow(10, token1_decimals)) as tvl_native, token1 as token from venomics.dex_reserves r
+  where r.token1 = '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+), native_based_pairs as (
+select id, token, max(tvl_native) as tvl_native from native_based_pairs_raw
+group by 1, 2
+), latest_prices as (
+  select address, build_time, price from top_tokens_datamart src
+  where build_time = (select max(build_time) from top_tokens_datamart ttd where src.address = ttd.address)
+), non_native_based_pairs_raw as (
+  select r.id, round(reserve0 * lp0.price / pow(10, ti0.decimals)) as tvl_native,
+  token0 as token
+  from venomics.dex_reserves r
+  join tokens_info ti0 on ti0.id = token0
+  left join latest_prices lp0 on lp0.address = token0
+  where token0 != '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+  and token1 != '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+  
+  union all 
+  
+  select r.id, round(reserve1 * lp1.price / pow(10, ti1.decimals)) as tvl_native,
+  token1 as token
+  from venomics.dex_reserves r
+  join tokens_info ti1 on ti1.id = token1
+  left join latest_prices lp1 on lp1.address = token1
+  where token0 != '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+  and token1 != '0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14'
+), non_native_based_pairs as (
+  select id, token, max(tvl_native) as tvl_native from non_native_based_pairs_raw
+  group by 1, 2
+), all_pools as (
+  select * from non_native_based_pairs
+  union all
+  select * from native_based_pairs
+)
+select * from all_pools
---
+++ etl/etl_datamart.py
@@ -0,0 +1,35 @@
+from airflow.decorators import dag, task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+from datetime import datetime, timedelta
+
+"""
+
+"""
+
+@dag(
+    schedule_interval="*/20 * * * *",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    concurrency=1,
+    max_active_runs=1,
+    tags=['venom', 'tokens', 'tvl', 'datamart']
+)
+def tvl_datamart():
+      tvl_history_entry = PostgresOperator(
+        task_id="tvl_history_entry",
+        postgres_conn_id="venom_db",
+        sql=[
+            """
+            insert into public.tvl_history(build_time, pool, 
+              token, reserve
+            )
+
+            select now(), id as pool, token, tvl_native as reserve
+            from venomics.tvl_current
+            """
+        ]
+    )
+
+
+tvl_datamart_dag = tvl_datamart()
